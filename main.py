e288ef9d0e60a2b014e0329eaebe2d87ea1d394f
Gabrielleite2 <gabrielleitebr002@gmail.com>
2023-06-19 19:27:20 -0300

    Adição da opção de Alterar o Título e Descrição de uma manifestação

--- main.py
+++ main.py
@@ -76,6 +76,33 @@ while opcao != 8:
             for i in pesquisa:
                 print('Código', i[0], '-', i[1], '-', i[4],
                       '-', i[3], '-', 'Descrição:', i[2], '-', 'Data:', i[5], '/', i[6], '/', i[7])
+    elif opcao == 6:
+        consulta = 'select count(*) from manifestacao'
+        manifestacao = listarBancoDados(conexao, consulta)
+        quantidade = manifestacao[0][0]
+        
+        if quantidade == 0:
+            print('Não existem manifestações cadastradas!')
+        else:
+            listar = 'select * from manifestacao'
+            manifestacao = listarBancoDados(conexao, listar)
+            for i in manifestacao:
+                print('Código', i[0], '-', i[1], '-', i[4], '-', i[3])
+            
+            codigo = input('Informe o código da manifestação que deseja alterar o título e descrição \nDigite: ')
+            consulta = 'select * from manifestacao where codigo = ' +codigo
+            resultado = listarBancoDados(conexao, consulta)
+            
+            if len(resultado) == 0:
+                print('Manifestação não encontrada!')
+            else:
+                novoTitulo = input('Digite o novo título: ')
+                novaDescricao = input('Digite a nova descrição: ')
+                sqlAtualizar = 'update manifestacao set titulo = %s, descricao = %s  where codigo = %s '
+                valores = [novoTitulo,novaDescricao, codigo]
+                atualizarBancoDados(conexao, sqlAtualizar, valores)
+                print('Manifestação editada com sucesso!')
+    
     elif opcao == 8:
         print('Sair')
         
